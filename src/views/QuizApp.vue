1e9169b7739370b6c24e4fd3fe8b33c8524e6d26
MayuHosokawa <mayuhskw422@gmail.com>
2023-05-24 23:53:03 +0900

    pullするため

--- src/views/QuizApp.vue
+++ src/views/QuizApp.vue
@@ -1,29 +1,108 @@
 <template>
   <h1>Vue クイズ</h1>
   <div class="app">
-    <h2>Q. {{ "クイズタイトル" }}</h2>
+    <h2 class="quiz-title">Q. {{ questions.title }}</h2>
     <img
       class="quiz-image"
       src="https://via.placeholder.com/300x300"
       alt="クイズタイトル"
     />
     <div class="container">
-      <button>
-        {{ "選択肢1" }}
-      </button>
-      <button>
-        {{ "選択肢2" }}
-      </button>
-      <button>
-        {{ "選択肢3" }}
+      <button
+        v-for="(choice, i) in question.answers"
+        v-bind:key="i"
+        v-on:click="answered(answer)"
+      >
+        {{ answer.text }}
       </button>
     </div>
-    <div>{{ "答え" }}</div>
+    <div>{{ feedback }}</div>
+    <button v-if="true">次の問題へ</button>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      feedback: "",
+      questions: [
+        {
+          title:"ひかるの出身は?",
+          answers:[
+            {
+            text:"山梨",
+            isCorrect: true,
+            feedback:"正解！"
+            },{
+            text:"山形",
+            isCorrect: false,
+            feedback:"残念！不正解！"
+            },{
+            text:"山口",
+            isCorrect: false,
+            feedback:"残念！不正解！"
+            }]
+
+
+        },
+        {
+          title:"美咲が一番好きなのは?",
+          answers:[
+            {
+            text:"白石麻衣",
+            isCorrect: false,
+            feedback:"残念！不正解！"
+            },{
+            text:"齋藤飛鳥",
+            isCorrect: false,
+            feedback:"残念！不正解！"
+            },{
+            text:"生田絵梨花",
+            isCorrect: true,
+            feedback:"正解！"
+            }]
+
+
+        },
+        {
+          title:"まゆの最寄り駅は?",
+          answers:[
+            {
+            text:"幕張本郷",
+            isCorrect: false,
+            feedback:"残念！不正解！"
+            },{
+            text:"海浜幕張",
+            isCorrect: true,
+            feedback:"正解！"
+            },{
+            text:"幕張豊砂",
+            isCorrect: false,
+            feedback:"残念！不正解！"
+            }]
+
+
+        },
+
+      ]
+  },
+  methods:{
+    answered(answeer){
+      this.feedback = answer.feedback
+
+      if(answer.isCorrect){
+
+      }
+    }
+  },
+},
+computed:{
+  questionUmagePath(){
+    return "./images/" + this.quiz.image
+  },
+},
+}
 </script>
 
 <style>
